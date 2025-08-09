437d07f30db83f550167f145f6cea3cd6063465a
devdiagon <fredericktimo@gmail.com>
2025-08-09 11:59:12 -0500

    added production urls

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrls: {
-    products: 'http://c-api-products:8003/api/products',
-    categories: 'http://c-api-categories:8004/api/categories'
+    products: 'https://product-api-production-2a47.up.railway.app/api/products',
+    categories: 'https://category-api-production-04f5.up.railway.app/api/categories'
   }
 };
